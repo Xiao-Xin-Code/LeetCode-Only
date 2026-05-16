805e52a9f21b36165bfd0f78eba1a04765ade220
liuyuhao <3284871599@qq.com>
2026-05-16 17:21:26 +0800

    feat(leetcode): 实现三数之和的解法
    实现了015题三数之和的标准双指针解法，先排序数组再通过双指针查找符合条件的三元组并去重

--- LeetCode/C++/Solution_001-100.h
+++ LeetCode/C++/Solution_001-100.h
@@ -318,8 +318,30 @@ namespace Solution {
 
 		//015-三数之和 https://leetcode.com/problems/3sum/
 		std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
-			
-			return {};
+			std::sort(nums.begin(), nums.end());
+			std::vector<std::vector<int>> result;
+			for(int i = 0; i < nums.size() - 2; ++i){
+				int target = -nums[i];
+				int left = i + 1;
+				int right = nums.size() - 1;
+				while(left < right){
+					int sum = nums[left] +nums[right];
+					if(sum == target){
+						result.push_back({nums[i], nums[left], nums[right]});
+						while(left < right && nums[left + 1] == nums[left]) left++;
+						while(left < right && nums[right - 1] == nums[right]) right--;
+						left++;
+						right--;
+					}
+					else if(sum < target){
+						left++;
+					}
+					else{
+						right--;
+					}
+				}
+			}
+			return result;
 		}
 
 	};
